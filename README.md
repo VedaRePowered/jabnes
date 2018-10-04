5d70826474aec96d5e05f0c7050ef7a466d5fcb7
BEN1JEN <benpheard@gmail.com>
2018-10-03 21:03:39 -0600

    Added stuff to the readme, and made an awesome screandump at startup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # JABNES (Just Another Basic Nintendo Emulation Software)
-jabnes is written by BEN1JEN in c++ and is currently under development
+jabnes is written by BEN1JEN in c++ and is currently under development.
+
+## details:
+```
++------------------+ JABNES p0.0.1:
+| J  A  B  N  E  S | - Creator: BEN1JEN
+| J  A  B  N  E  S | - Creator: BEN1JEN
+| u  n  a  i  m  o | - Licence: GPL 3.0
+| s  o  s  n  u  f | - Ganra: Emulator
+| t  t  i  t  l  t | - - System: NES
+|    h  c  e  a  w | - - Supported ROMs: none
+|    e     n  t  a | - - Emulated: CPU (kinda)
+|    r     d  i  r | - Build Type: pre-alpha
+|          o  o  e | - Platforms: Linux
+|             n    | - Git Repo: https://gitlab.101100.ca/ben1jen/jabnes
++------------------+ - \/Info:\/
+JABNES is an NES emulator for linux written in c++ with no set goals in mind.
+It is unlikly to ever suppert any actual ROMs, but is in constant development.
+NOTE: this project does not have base features as indicated by the p before the version.
+```
--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <cstddef>
+#include <iostream>
 
 #include "instructions.h"
 
 int main() {
+	std::cout << "+------------------+ JABNES p0.0.1:" << std::endl;
+	std::cout << "| J  A  B  N  E  S | - Creator: BEN1JEN" << std::endl;
+	std::cout << "| u  n  a  i  m  o | - Licence: GPL 3.0" << std::endl;
+	std::cout << "| s  o  s  n  u  f | - Ganra: Emulator" << std::endl;
+	std::cout << "| t  t  i  t  l  t | - - System: NES" << std::endl;
+	std::cout << "|    h  c  e  a  w | - - Supported ROMs: none" << std::endl;
+	std::cout << "|    e     n  t  a | - - Emulated: CPU (kinda)" << std::endl;
+	std::cout << "|    r     d  i  r | - Has Base Features: No" << std::endl;
+	std::cout << "|          o  o  e | - Build Type: pre-alpha" << std::endl;
+	std::cout << "|             n    | - Platforms: Linux" << std::endl;
+	std::cout << "+------------------+ - Git Repo: https://gitlab.101100.ca/ben1jen/jabnes" << std::endl;
+	std::cout << "\033[53mLog Output:                                                             \033[0m" << std::endl;
+
 	return 0;
 }
