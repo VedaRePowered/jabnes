0917d8d9968b69ad273a47aaaba1973c3aca4442
BEN1JEN <benpheard@gmail.com>
2018-10-03 19:28:39 -0600

    update state class for memory map

--- state.cpp
+++ state.cpp
@@ -1,11 +1,38 @@
 #include "state.h"
 
 unsigned short int reg_dummy;
+int last_joy_1_read = -1;
+int last_joy_2_read = -1;
 
 state::state() {
+	// build an array for mapping nes cpu memory
+	for (int i=0x0000; i <= 0x07FF; i++) {
+		this->cpu_memory_map[i+0x0800*0] = &(ram[i]);
+		this->cpu_memory_map[i+0x0800*1] = &(ram[i]);
+		this->cpu_memory_map[i+0x0800*2] = &(ram[i]);
+		this->cpu_memory_map[i+0x0800*3] = &(ram[i]);
+	}
+	for (int i=0; i <= 7; i++) {
+		for (int o; o < 1024; o+= 8) {
+			this->cpu_memory_map[0x2000+i+o] = &(ppu_regs[i]);
+		}
+	}
+	for (int i=0x4000; i <= 0x40016; i++) {
+		this->cpu_memory_map[i] = &(apu_regs[i-0x4000]);
+	}
+	for (int i=0x4017; i <= 0x401F; i++) {
+		this->cpu_memory_map[i] = &reg_dummy; // not emulating CPU test mode, and joysticks are a special case in the memory getting function
+	}
+	for (int i=0x4020; i <= 0xFFFF; i++) {
+		this->cpu_memory_map[i] = &(cart_mem[i-4020]);
+	}
+
 	this->reg_a = 0;
 	this->reg_x = 0;
 	this->reg_y = 0;
+	this->reg_s = 0;
+	this->reg_p = 0;
+	this->reg_pc = 0xC000; // start position for nestest
 }
 
 unsigned short int * state::get_reg(char reg) {
@@ -13,13 +40,31 @@ unsigned short int * state::get_reg(char reg) {
 		case 'a': return &reg_a;
 		case 'x': return &reg_x;
 		case 'y': return &reg_y;
+		case 's': return &reg_s;
+		case 'p': return &reg_p;
+		case 'c': return &reg_pc;
 	}
 	return &reg_dummy;
 }
 void state::set_reg(char reg, unsigned short int val) {
-	switch(reg) {
-		case 'a': reg_a = val;
-		case 'x': reg_x = val;
-		case 'y': reg_y = val;
+	val = val & 0x00FF;
+	*(get_reg(reg)) = val;
+}
+
+unsigned short int * state::get_memory(unsigned short int loc) {
+	if(loc == 0x4017) {
+		return (unsigned short int)0;
+	} else if (loc == 0x4018) {
+		return (unsigned short int)0;
+	} else {
+		return cpu_memory_map[loc];
+	}
+}
+
+void state::set_memory(unsigned short int loc, unsigned short int val) {
+	if(loc == 0x4017) {
+	} else if (loc == 0x4018) {
+	} else {
+		*(get_memory(loc)) = val;
 	}
 }
--- state.h
+++ state.h
@@ -1,5 +1,5 @@
-#ifndef INSTRUCTIONS_H
-#define INSTRUCTIONS_H
+#ifndef STATE_H
+#define STATE_H
 
 #include <cstddef>
 
@@ -10,10 +10,27 @@ public:
 	state();
 	unsigned short int * get_reg(char reg);
 	void set_reg(char reg, unsigned short int val);
+	unsigned short int * get_memory(unsigned short int loc);
+	void set_memory(unsigned short int loc, unsigned short int val);
 private:
 	unsigned short int reg_a;
 	unsigned short int reg_x;
 	unsigned short int reg_y;
+	unsigned short int reg_s;
+	unsigned short int reg_p;
+	unsigned short int reg_pc;
+	unsigned short int ram[0x0800];
+	unsigned short int apu_regs[0x0016];
+	unsigned short int ppu_regs[0x0008];
+	unsigned short int cart_mem[0xBFE0];
+	unsigned short int pattern_table[2][0x1000];
+	unsigned short int name_table[4][0x0400];
+	unsigned short int palette_ram[0x0020];
+	unsigned short int oam_spr_x[0x0040];
+	unsigned short int oam_spr_y[0x0040];
+	unsigned short int oam_spr_n[0x0040];
+	unsigned short int oam_spr_a[0x0040];
+	unsigned short int * cpu_memory_map[0x10000];
 };
 
 
